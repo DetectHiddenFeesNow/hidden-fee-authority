a1acee71602b588f4c0ea70bdf085faa9f692b07
Sentinel <sentinel@detecthiddenfees.com>
2026-07-25 18:54:46 -0500

    Clean banking supporting pages with proper headings

---
+++ scripts/gen_banking3.js
@@ -0,0 +1,2 @@
+g("hidden-overdraft-fees","Hidden Overdraft Fees","Overdraft fee guide");
+g("hidden-atm-fees","Hidden ATM Fees","ATM fee guide");g("hidden-checking-account-fees","Hidden Checking Account Fees","Checking fee guide");g("hidden-wire-transfer-fees","Hidden Wire Transfer Fees","Wire fee guide");g("hidden-bank-maintenance-fees","Hidden Bank Maintenance Fees","Maintenance fee guide");
